332278511ca4f84efbd9298bf2ee902ab4eb91a6
Ankit Kr Chowdhury <rakesh856100@gmail.com>
2025-03-29 17:16:20 +0530

    Enhance OurUses component with Framer Motion for improved hover animations and transitions

--- components/our-uses.tsx
+++ components/our-uses.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { PlusIcon } from "lucide-react";
+import { motion } from "framer-motion";
 import {
   SectionWrapper,
   SectionContentWrapper,
@@ -47,24 +48,54 @@ export default function OurUses() {
 
 function UseItem({ index, text, description }: UseItemProps) {
   return (
-    <div className="w-full py-6 rounded-lg bg-transparent flex justify-between items-center">
-      <div className="flex items-center justify-start gap-x-16">
-        <h1 className="text-dark font-medium font-onest text-4xl tracking-[-0.158rem]">
+    <motion.div
+      whileHover="hover"
+      initial="initial"
+      className="w-full py-6 rounded-lg bg-transparent flex justify-between items-center transition-colors duration-300"
+      variants={{
+        initial: { backgroundColor: "transparent" },
+        hover: { backgroundColor: "var(--primary-blue)" },
+      }}
+    >
+      <motion.div
+        variants={{
+          initial: { x: 0, color: "#0c120c" },
+          hover: { x: 20, color: "#ffffff" }, // Move right & turn white
+        }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+        className="flex items-center justify-start gap-x-28"
+      >
+        <h1 className="font-medium font-onest text-4xl tracking-[-0.158rem]">
           0{index + 1}
         </h1>
 
-        <h1 className="text-dark font-medium font-onest text-[4.5rem] tracking-[-0.315rem]">
+        <h1 className="font-medium font-onest text-[4.5rem] tracking-[-0.315rem] leading-[4.95rem]">
           {text}
         </h1>
-      </div>
+      </motion.div>
 
-      <div className="flex items-center justify-start gap-x-8">
-        <p className="text-base sm:text-lg xl:text-xl text-body font-onest tracking-[-0.023rem] leading-6  xl:w-[610px] font-medium">
+      <motion.div className="flex items-center justify-start gap-x-8 relative">
+        <motion.p
+          variants={{
+            initial: { opacity: 0, x: 0, color: "#0c120c" },
+            hover: { opacity: 1, x: -20, color: "#ffffff" },
+          }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+          className="text-base sm:text-lg xl:text-xl font-onest tracking-[-0.023rem] leading-6 xl:w-[610px] font-medium"
+        >
           {description}
-        </p>
+        </motion.p>
 
-        <PlusIcon className="size-10" />
-      </div>
-    </div>
+        <motion.div
+          variants={{
+            initial: { opacity: 1, x: 0, color: "#0c120c" },
+            hover: { opacity: 0, x: -20, color: "#ffffff" },
+          }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+        >
+          <PlusIcon className="size-10" />
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 }
